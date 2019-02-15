bae436622438d12994b714976b9698620861a2ca
ekas <ekaspreet93.singh@gmail.com>
2019-02-15 23:38:26 +0530

    Updated UI Design

--- src/App.vue
+++ src/App.vue
@@ -9,15 +9,15 @@
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Ubuntu:400,500,700");
 * {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
+  font-family: "Ubuntu", sans-serif;
 }
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 </style>
--- src/components/QuizComponent.vue
+++ src/components/QuizComponent.vue
@@ -2,16 +2,33 @@
   <div class="container">
     <div class="quiz_header">
       <div class="quiz_quest_icon">
-        <i class="fab fa-accessible-icon"></i>
+        <img src="../assets/quizImg_1.svg" alt>
       </div>
-      <div class="quiz_quest_header">
-        <h1>Do you smoke cigarettes on Daily basis?</h1>
+      <div class="quiz_quest_text">
+        <p>Do you party very often?</p>
       </div>
     </div>
-    <div class="quiz_choices"></div>
+    <div class="quiz_choice_container">
+      <span class="quiz_choices">
+        <span class="choice_option">A</span>
+        <span class="choice_text">Yes</span>
+        <span></span>
+      </span>
+      <span class="quiz_choices">
+        <span class="choice_option">B</span>
+        <span class="choice_text">No</span>
+        <span class="choice_selected">
+          <i class="fas fa-check-circle"></i>
+        </span>
+      </span>
+    </div>
     <div class="quiz_footer">
       <div class="footer_container">
-        <div class="footer_progress_bar"></div>
+        <div class="footer_progress_bar_container">
+          <div class="footer_progress_bar">
+            <div class="footer_progress"></div>
+          </div>
+        </div>
         <div class="footer_buttons">
           <button class="btn_prev">
             <i class="fas fa-angle-left"></i>
@@ -37,14 +54,70 @@ export default {
   background-color: #f7f8fc;
   height: 100vh;
   width: 100vw;
-  overflow: hidden;
 }
+
+/*Header*/
 .quiz_header {
   text-align: center;
-  height: calc(100% - 80px);
+  /*height: calc(100% - 80px);*/
   width: 100%;
-  overflow-y: scroll;
+  padding: 100px 0 0;
+
+  .quiz_quest_icon img {
+    height: 100px;
+  }
+
+  .quiz_quest_text p {
+    color: #21232c;
+    font-size: 35px;
+    font-weight: 600;
+    word-spacing: 1.5px;
+    letter-spacing: 0.5px;
+    line-height: 46px;
+  }
 }
+
+/* Body */
+.quiz_choice_container {
+  padding: 50px 0 0;
+  text-align: center;
+
+  .quiz_choices {
+    background-color: #fff;
+    border: solid 1.5px #5861ae;
+    letter-spacing: -0.5px;
+    padding: 13px 44px 13px 12px;
+    color: #21232c;
+    width: 125px;
+    margin: 0 auto;
+    border-radius: 9px;
+    font-size: 15px;
+    font-weight: 600;
+    -webkit-box-shadow: 0px 0px 0px 4px rgba(223, 224, 239, 1);
+    -moz-box-shadow: 0px 0px 0px 4px rgba(223, 224, 239, 1);
+    box-shadow: 0px 0px 0px 4px rgba(223, 224, 239, 1);
+    margin-left: 20px;
+    position: relative;
+  }
+  .choice_option {
+    border: solid 1px #c6c9d2;
+    color: #c6c9d2;
+    padding: 2px 5px;
+    border-radius: 3px;
+    margin-right: 5px;
+  }
+
+  .choice_selected {
+    position: absolute;
+    right: 10px;
+    top: 11px;
+    font-size: 20px;
+    color: #5861ae;
+  }
+}
+
+/* Footer */
+
 .quiz_footer {
   width: 100%;
   height: 80px;
@@ -55,32 +128,57 @@ export default {
   -webkit-box-shadow: -1px -6px 9px -3px rgba(223, 225, 239, 0.85);
   -moz-box-shadow: -1px -6px 9px -3px rgba(223, 225, 239, 0.85);
   box-shadow: -1px -6px 9px -3px rgba(223, 225, 239, 0.85);
-}
 
-/* Footer */
-.footer_container {
-  padding: 10px 20px;
+  .footer_container {
+    padding: 10px 20px;
+
+    .footer_progress_bar_container {
+      width: 500px;
+      padding: 24px 0;
+      float: left;
+
+      .footer_progress_bar {
+        background-color: #ecedf5;
+        width: 100%;
+        height: 14px;
+        border-radius: 7px;
+        position: relative;
+      }
+
+      .footer_progress {
+        position: absolute;
+        background-color: #5861ae;
+        width: 50%;
+        height: 14px;
+        border-radius: 7px;
+      }
+    }
+  }
 
   .footer_buttons {
     margin-top: 11px;
     float: right;
-  }
-  .footer_buttons button {
-    padding: 10px 15px;
-    border: solid 1px #dfe1ef;
-    background-color: #fff;
-  }
-  .footer_buttons button i {
-    color: #8d94a4;
-    font-size: 20px;
-  }
-  .btn_prev {
-    border-top-left-radius: 5px;
-    border-bottom-left-radius: 5px;
-  }
-  .btn_next {
-    border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
+
+    button {
+      padding: 10px 15px;
+      border: solid 1px #dfe1ef;
+      background-color: #fff;
+      margin: 0;
+
+      .btn_prev {
+        border-top-left-radius: 5px;
+        border-bottom-left-radius: 5px;
+      }
+
+      .btn_next {
+        border-top-right-radius: 5px;
+        border-bottom-right-radius: 5px;
+      }
+      i {
+        color: #8d94a4;
+        font-size: 20px;
+      }
+    }
   }
 }
 </style>
